a44e1c9d4c0861bdb98f62264fd53d4b2c59951e
marble <florian.renard@spectralgo.com>
2026-04-29 13:03:45 +0100

    feat(desktop/MailPane): wrap MailPanel in PaneErrorBoundary (ss-i46i)

--- apps/desktop/src/renderer/components/Gastown/MailPane/MailPane.tsx
+++ apps/desktop/src/renderer/components/Gastown/MailPane/MailPane.tsx
@@ -1,4 +1,5 @@
 import { MailPanel } from "renderer/components/Gastown/MailPanel";
+import { PaneErrorBoundary } from "renderer/components/Shell/PaneErrorBoundary";
 import type { BaseTab, Pane } from "shared/tabs-types";
 
 interface MailPaneProps {
@@ -12,7 +13,9 @@ export function MailPane(_props: MailPaneProps) {
 			data-mail-root
 			className="flex h-full w-full min-w-[320px] flex-col bg-background"
 		>
-			<MailPanel />
+			<PaneErrorBoundary>
+				<MailPanel />
+			</PaneErrorBoundary>
 		</div>
 	);
 }
